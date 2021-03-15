226e4bbd856d655d29b3d58d849e5ca3aa5bde29
Daniel Lienert <daniel@lienert.cc>
2021-03-15 14:25:01 +0100

    BUGFIX: Send correct response header

--- Classes/FusionObject/FeedImplementation.php
+++ Classes/FusionObject/FeedImplementation.php
@@ -8,13 +8,43 @@ namespace PunktDe\FusionFeed\FusionObject;
  *  All rights reserved.
  */
 
+use GuzzleHttp\Psr7\Message;
+use Neos\Flow\Annotations as Flow;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
 use Suin\RSSWriter\Channel;
 use Suin\RSSWriter\Feed;
 
 class FeedImplementation extends AbstractFeedElement
 {
+    /**
+     * @Flow\Inject
+     * @var StreamFactoryInterface
+     */
+    protected $contentStreamFactory;
+
+    /**
+     * @Flow\Inject
+     * @var ResponseFactoryInterface
+     */
+    protected $responseFactory;
 
     protected string $elementClassName = Feed::class;
+
+    public function evaluate()
+    {
+        /** @var Feed $feed */
+        $feed = parent::evaluate();
+
+        $response = $this->responseFactory->createResponse()
+            ->withHeader('content-type', 'application/atom+xml');
+
+        $contentStream = $this->contentStreamFactory->createStream($feed->render());
+        $response = $response->withBody($contentStream);
+
+        return Message::toString($response);
+    }
+
 
     protected function getChannel(): void
     {
